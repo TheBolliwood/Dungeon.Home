fced324580229cd05ae9b221c6cbc51a62936072
Martin - "sha256_ <jurranmartin@gmail.com>
2017-06-03 18:05:40 +0200

    Styled repository-section
    I finally done some basic styling of the repository-section. Icons and Stats have to follow.

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 app = {
+  repoURI:"https://github.com/DungeonChat/",
+  repoPrefix:"Dungeon.",
+
   init: function () {
     var str_username = "DungeonChat";
     app.getRepos(str_username);
@@ -24,22 +27,24 @@ app = {
     $.getJSON( "https://api.github.com/users/" + str_username + "/repos", function( data ) {
 
       for (index = 0; index < data.length; ++index) {
-          console.log(data[index]);
 
-          //filling minified info array
-          arr_miniRepoInfo[index] = {
-            name: data[index]['name'],
-            forks: data[index]['forks'],
-            stars: data[index]['stargazers_count'],
-            watchers: data[index]['watchers']
+          if(data[index]['name'].indexOf(app.repoPrefix) !== -1){
+            //filling minified info array
+            arr_miniRepoInfo[index] = {
+              name: data[index]['name'],
+              forks: data[index]['forks'],
+              stars: data[index]['stargazers_count'],
+              watchers: data[index]['watchers'],
+              uri: data[index]['html_url']
+            }
           }
       }
+
       app.renderRepo(arr_miniRepoInfo);
 
     });
   },
   renderRepo: function(arr_miniRepoInfo){
-    var repoPrefix = "Dungeon.";
     var rowCount = Math.round(app.getObjSize(arr_miniRepoInfo) / 3);
     console.log(rowCount);
 
@@ -54,13 +59,15 @@ app = {
         ++renderedRowCount
       }
 
-      var repo_name = arr_miniRepoInfo[index]['name'].replace(repoPrefix, "");
+      var repo_name = arr_miniRepoInfo[index]['name'].replace(app.repoPrefix, "");
       var repo_forks = arr_miniRepoInfo[index]['forks'];
       var repo_stars = arr_miniRepoInfo[index]['stars'];
       var repo_watchers = arr_miniRepoInfo[index]['watchers'];
+      var repo_uri = arr_miniRepoInfo[index]['uri']
 
-       $("#" + (renderedRowCount - 1)).append("<div class='three columns'>Name:" + repo_name + "<br/>Forks" + repo_forks + "<br/>Stars" + repo_stars + "<br/>Watchers" + repo_watchers + "</div>");
+       $("#" + (renderedRowCount - 1)).append('<div class="four columns repo_item"><h3>' + repo_name + '</h3>Forks' + repo_forks + '<br/>Stars' + repo_stars + '<br/>Watchers' + repo_watchers + '<a href="' + repo_uri + '" target="_blank">Visit<i class="ion-android-open"></i></a></div>');
 
     }
+
   }
 }
